8d00c46b4124373656df060fa3817f32aa137556
lucapcf <lucapcf@gmail.com>
2024-08-26 19:06:13 -0300

    Adding dwm and .fehbg

--- .bashrc
+++ .bashrc
@@ -9,12 +9,30 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 
+# Unlimited shell history
+export HISTFILESIZE=
+export HISTSIZE=
 
-# la
+# Recording time
+HISTTIMEFORMAT="%d/%m/%y %T "
+
+# List all files
 alias la='ls -a'
 
+# Remove display
+alias removedisplay='xrandr --output HDMI-1 --off'
 
+# Link display
 alias linkdisplay='xrandr --output HDMI-1 --mode 1920x1080  --right-of eDP-1'
 
 # Default Browser
 export BROWSER=/usr/bin/firefox
+
+# vi mode
+set -o vi
+
+##-----------------------------------------------------
+# synth-shell-prompt.sh
+if [ -f /home/lucapcf/.config/synth-shell/synth-shell-prompt.sh ] && [ -n "$( echo $- | grep i )" ]; then
+	source /home/lucapcf/.config/synth-shell/synth-shell-prompt.sh
+fi
---
+++ dwm/scripts/status.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Function to get current time
+dwm_status() {
+    while true; do
+        # Get the current time and format it as you wish
+        TIME=$(date +"%D | %T")
+
+	# Get the battery info
+	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+	BAT_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+	
+	# Get the volume level
+	VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' |     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+	
+	# Limits max volume
+	if [ "$VOLUME" -gt 150 ]; then
+		pactl set-sink-volume 0 150%
+	fi
+
+	# Extract memory data
+        TOTAL_KB=$(awk '/MemTotal:/ {print $2}' /proc/meminfo)
+        FREE_KB=$(awk '/MemFree:/ {print $2}' /proc/meminfo)
+        BUFFERS_KB=$(awk '/Buffers:/ {print $2}' /proc/meminfo)
+        CACHED_KB=$(awk '/^Cached:/ {print $2}' /proc/meminfo)
+
+	# Calculate indicators
+	TOTAL_GB=$(bc <<< "scale=2; $TOTAL_KB / 1024 / 1024")
+	USED_GB=$(bc <<< "scale=2; ($TOTAL_KB - $FREE_KB - $BUFFERS_KB - $CACHED_KB) / 1024 / 1024")
+	
+	# Ensure used RAM is displayed with two decimal places
+        FORMATTED_USED_GB=$(printf "%.2f" "$USED_GB")
+	
+        # Update dwm status bar
+	xsetroot -name "Total RAM: $TOTAL_GB GB | Used RAM: $FORMATTED_USED_GB GB | Volume: $VOLUME | $BAT% and $BAT_STATUS | $TIME"
+
+        # Wait for 1 second before updating again
+        sleep 1
+    done
+}
+
+dwm_status &
